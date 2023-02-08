00fa8ab504a3c7ea74c5363b3361a2afeda94f71
Pak <helicopterpak@gmail.com>
2023-02-08 16:55:31 +0300

    Task2 done

--- src/main/kotlin/Person.kt
+++ src/main/kotlin/Person.kt
@@ -1,19 +1,18 @@
-//import jdk.javadoc.internal.doclint.HtmlTag.get
-//
-//data class Person(
-//    val name: String,
-//    val surname: String,
-//    var age: Int
-//)
-//
-//fun main() {
-//    val person = mutableListOf<Person>(Person("Tony", "Hawk", 80),
-//        Person("Tony", "Braxton", 90),
-//        Person("Roberto", "Carlos", 70),
-//        Person("Pit", "Davis", 100)
-//    )
-//    println(person.sortByAge())
-//}
-//fun <Person> MutableList<Person>.sortByAge(mutableList: MutableList<Person>){
-//    this.sortByDescending{it.age}
-//}
+data class Person(
+    val name: String,
+    val surname: String,
+    var age: Int
+)
+
+fun main() {
+    val person = mutableListOf<Person>(Person("Tony", "Hawk", 80),
+        Person("Tony", "Braxton", 90),
+        Person("Roberto", "Carlos", 70),
+        Person("Pit", "Davis", 100)
+    )
+    println(person.sortByAge())
+    println("~~~")
+    println(person.sortByNameAndSurname())
+}
+fun List<Person>.sortByAge(): List<Person> = sortedByDescending{it.age}
+fun List<Person>.sortByNameAndSurname(): List<Person> = sortedBy {it.surname}.sortedBy { it.name }
